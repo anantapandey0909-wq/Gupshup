aa14b852175455161c7093331bf464ad1cd231e4
Ananta Pandey <anantapandey@placeholder.com>
2026-04-05 18:09:44 +0530

    Fix host binding for Render to fix crashing

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('*', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(`Local: http://localhost:${PORT}`);
 });
